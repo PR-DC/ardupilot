336eacbfd0b620a70b6054b003127f61a4a34151
Miloš Petrašinović <petrasinovicmp@gmail.com>
2021-12-07 11:06:59 +0100

    AP_Periph: Update version fix

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -1,14 +1,11 @@
 #pragma once
 
-#define THISFIRMWARE "PR-DC AP_Periph V1.3dev"
+#define THISFIRMWARE "PR-DC AP_Periph V1.2"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,3,0,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 1,2,90,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #define FW_MAJOR 1
-#define FW_MINOR 3
-#define FW_PATCH 90 // PR-DC from 90
-#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
-
-
-
+#define FW_MINOR 2
+#define FW_PATCH 90
+#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
